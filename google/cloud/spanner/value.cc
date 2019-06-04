88331bc8ea2bcbd8680246d73689ad17e9d82199
Greg Miller <9447643+devjgm@users.noreply.github.com>
2019-06-04 10:15:37 -0400

    Refactoring to make it easier to add spanner ARRAY support.
    Removed the switch statement in operator==() in favor of a custom
    protobuf message differencer, which will grow/expand with any
    data/values that we stuff in the proto. A custom differencer was needed
    so that we could make encoded "NaN" values still appear unequal to each
    other, as they're supposed to.
    
    Removed the switch statement in operator<<() and changed to directly
    printing the internal protobuf contents with `.ShortDebugString()`.
    Since this format exposes internal implementation details that we don't
    want external users or computers to depend on, I changed op<< ->
    PrintTo() to make it clearer that this "formatting" is only inteded for
    nice output during failed gtest assertions.
    
    Changed the IsType() functions that used to return bool to instead
    return the type code so that it can later be used in other places. Also
    refactored those functions into a small, private TypeCodeMap struct.
    
    	modified:   value.cc
    	modified:   value.h
    	modified:   value_test.cc

--- google/cloud/spanner/value.cc
+++ google/cloud/spanner/value.cc
@@ -14,6 +14,8 @@
 
 #include "google/cloud/spanner/value.h"
 #include "google/cloud/log.h"
+#include <google/protobuf/util/field_comparator.h>
+#include <google/protobuf/util/message_differencer.h>
 #include <cmath>
 #include <ios>
 #include <string>
@@ -34,6 +36,29 @@ Status CheckValidity(Value const& v) {
   if (v.is_null()) return Status(StatusCode::kInvalidArgument, "null value");
   return {};  // OK status
 }
+
+// A custom protobuf comparator to make Spanner-encoded FLOAT64 "NaN" values
+// compare not equal to each other. This is the behavior required by IEEE 754.
+class CustomComparator : public google::protobuf::util::DefaultFieldComparator {
+ public:
+  ComparisonResult Compare(
+      const google::protobuf::Message& message_1,
+      const google::protobuf::Message& message_2,
+      const google::protobuf::FieldDescriptor* field, int index_1, int index_2,
+      const google::protobuf::util::FieldContext* field_context) override {
+    auto const* const reflection = message_1.GetReflection();
+    // Spanner requires that FLOAT64 values (i.e., double) that represent nan
+    // are stored as the string "NaN". But IEEE 754 requires that NaN compares
+    // not equal to anything, including itself.
+    if (field->name() == "string_value") {
+      if (reflection->GetString(message_1, field) == "NaN") {
+        return DIFFERENT;
+      }
+    }
+    return DefaultFieldComparator::Compare(message_1, message_2, field, index_1,
+                                           index_2, field_context);
+  }
+};
 
 }  // namespace
 
@@ -64,67 +89,18 @@ Value::Value(std::string v) {
 }
 
 bool operator==(Value a, Value b) {
-  if (a.type_.code() != b.type_.code()) return false;
-  if (a.is_null() != b.is_null()) return false;
-  if (a.is_null()) return true;  // They are both null
-  switch (a.type_.code()) {
-    case google::spanner::v1::TypeCode::BOOL:
-      return *a.get<bool>() == *b.get<bool>();
-    case google::spanner::v1::TypeCode::INT64:
-      return *a.get<std::int64_t>() == *b.get<std::int64_t>();
-    case google::spanner::v1::TypeCode::FLOAT64:
-      return *a.get<double>() == *b.get<double>();
-    case google::spanner::v1::TypeCode::STRING:
-      return *a.get<std::string>() == *b.get<std::string>();
-    default:
-      return false;
-  }
+  google::protobuf::util::MessageDifferencer diff;
+  CustomComparator comp;
+  diff.set_field_comparator(&comp);
+  return diff.Compare(a.type_, b.type_) && diff.Compare(a.value_, b.value_);
 }
 
-std::ostream& operator<<(std::ostream& os, Value v) {
-  auto const& code = v.type_.code();
-  auto const* const descriptor = google::spanner::v1::TypeCode_descriptor();
-  os << "Value{" << descriptor->FindValueByNumber(code)->name() << ":";
-  if (v.is_null()) return os << "(null)}";
-  switch (code) {
-    case google::spanner::v1::TypeCode::BOOL:
-      os << *v.get<bool>();
-      break;
-    case google::spanner::v1::TypeCode::INT64:
-      os << *v.get<std::int64_t>();
-      break;
-    case google::spanner::v1::TypeCode::FLOAT64:
-      os << *v.get<double>();
-      break;
-    case google::spanner::v1::TypeCode::STRING:
-      os << *v.get<std::string>();
-      break;
-  }
-  return os << "}";
+void PrintTo(Value const& v, std::ostream* os) {
+  *os << v.type_.ShortDebugString() << "; " << v.value_.ShortDebugString();
 }
 
 bool Value::is_null() const {
   return value_.kind_case() == google::protobuf::Value::kNullValue;
-}
-
-//
-// Value::IsType
-//
-
-bool Value::IsType(bool) const {
-  return type_.code() == google::spanner::v1::TypeCode::BOOL;
-}
-
-bool Value::IsType(std::int64_t) const {
-  return type_.code() == google::spanner::v1::TypeCode::INT64;
-}
-
-bool Value::IsType(double) const {
-  return type_.code() == google::spanner::v1::TypeCode::FLOAT64;
-}
-
-bool Value::IsType(std::string) const {
-  return type_.code() == google::spanner::v1::TypeCode::STRING;
 }
 
 //
