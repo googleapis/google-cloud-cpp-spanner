28c89741f33d19dcd2e35a79f92ba1b642c8308f
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-06-26 13:10:18 -0700

    Automatically generated documentation

--- latest/search/functions_2.js
+++ latest/search/functions_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['databaseadminclient',['DatabaseAdminClient',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminClient.html#abdd9620e892e6be196c2abc1840eb40f',1,'google::cloud::spanner::v0::DatabaseAdminClient']]],
+  ['databaseadminclient',['DatabaseAdminClient',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminClient.html#abdd9620e892e6be196c2abc1840eb40f',1,'google::cloud::spanner::v0::DatabaseAdminClient::DatabaseAdminClient(ClientOptions const &amp;client_options=ClientOptions())'],['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminClient.html#a6c97182b1d2d562a7f09c42599e31275',1,'google::cloud::spanner::v0::DatabaseAdminClient::DatabaseAdminClient(std::shared_ptr&lt; internal::DatabaseAdminStub &gt; s)']]],
   ['dropdatabase',['DropDatabase',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminClient.html#a91faa307a79abe7867622e310d249ad9',1,'google::cloud::spanner::v0::DatabaseAdminClient']]]
 ];
