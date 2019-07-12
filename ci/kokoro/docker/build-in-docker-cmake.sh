2bf6aa0a41caf27b52ad4d09d0334964afaec558
Tommy Nguyen <remyabel@gmail.com>
2019-07-12 13:49:31 -0400

    Fix install prefix for non-install build.

--- ci/kokoro/docker/build-in-docker-cmake.sh
+++ ci/kokoro/docker/build-in-docker-cmake.sh
@@ -58,7 +58,8 @@ if [[ "${CODE_COVERAGE:-}" == "yes" ]]; then
     "-DCMAKE_BUILD_TYPE=Coverage")
 fi
 
-cmake "-H${SOURCE_DIR}" "-B${BINARY_DIR}" "${cmake_flags[@]}"
+cmake "-DCMAKE_INSTALL_PREFIX=$HOME/staging" \
+      "-H${SOURCE_DIR}" "-B${BINARY_DIR}" "${cmake_flags[@]}"
 cmake --build "${BINARY_DIR}" -- -j "$(nproc)"
 
 # When user a super-build the tests are hidden in a subdirectory. We can tell
