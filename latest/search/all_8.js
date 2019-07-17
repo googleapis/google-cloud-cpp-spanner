e2234f48ba412e4837d8e78d3a4e880baa660ce5
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-07-17 11:12:39 -0700

    Automatically generated documentation

--- latest/search/all_8.js
+++ latest/search/all_8.js
@@ -1,12 +1,12 @@
 var searchData=
 [
   ['main',['main',['../database__admin__samples_8cc.html#adacbe0175a79dff748855d8c9839f82b',1,'database_admin_samples.cc']]],
-  ['makeinsertmutation',['MakeInsertMutation',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#a3d5442587646ffaacaa093f3a3158a4a',1,'google::cloud::spanner::v0']]],
-  ['makeinsertorupdatemutation',['MakeInsertOrUpdateMutation',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#a9abfbfdf4c2c069db58b8f6e6cae35d5',1,'google::cloud::spanner::v0']]],
+  ['makeinsertmutation',['MakeInsertMutation',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#af77b5f3c1a45e35f626398888df17610',1,'google::cloud::spanner::v0']]],
+  ['makeinsertorupdatemutation',['MakeInsertOrUpdateMutation',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#a7170284d5638df130d97ec16318e9dac',1,'google::cloud::spanner::v0']]],
   ['makenullvalue',['MakeNullValue',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#ae3b5712ab2b1add6e7e610effc5f35b4',1,'google::cloud::spanner::v0']]],
-  ['makereplacemutation',['MakeReplaceMutation',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#a54b49f935babd07aa4b3db9d76a5a7fd',1,'google::cloud::spanner::v0']]],
+  ['makereplacemutation',['MakeReplaceMutation',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#a23d60b161bb989e8d1f299d45ed0f741',1,'google::cloud::spanner::v0']]],
   ['makerow',['MakeRow',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#af19702e3242db4899b9e2d8f974565af',1,'google::cloud::spanner::v0']]],
-  ['makeupdatemutation',['MakeUpdateMutation',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#a419d013582c1ffd3290f41065d4dc1b9',1,'google::cloud::spanner::v0']]],
+  ['makeupdatemutation',['MakeUpdateMutation',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#a0c3def39cf255dbb69ebf246930199f5',1,'google::cloud::spanner::v0']]],
   ['month',['month',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Date.html#a6bb58d11955b85ac06de2948760e62ee',1,'google::cloud::spanner::v0::Date']]],
   ['mutation',['Mutation',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Mutation.html',1,'google::cloud::spanner::v0::Mutation'],['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Mutation.html#a311f18265080cd893771a7705df91d17',1,'google::cloud::spanner::v0::Mutation::Mutation()=default'],['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Mutation.html#aa52eff25a8ecc973122d2f5fea2ad6b7',1,'google::cloud::spanner::v0::Mutation::Mutation(Mutation &amp;&amp;) noexcept=default'],['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Mutation.html#a2bc59b7bcb336837dc2250da70ed2454',1,'google::cloud::spanner::v0::Mutation::Mutation(Mutation const &amp;)=default']]],
   ['mutations_2ecc',['mutations.cc',['../mutations_8cc.html',1,'']]],
