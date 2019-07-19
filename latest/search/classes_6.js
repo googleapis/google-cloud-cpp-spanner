be02ba0c838e08174dcabb69a2306b1748d85e5a
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-07-19 12:13:23 -0700

    Automatically generated documentation

--- latest/search/classes_6.js
+++ latest/search/classes_6.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['resultset',['ResultSet',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1ResultSet.html',1,'google::cloud::spanner::v0']]],
   ['row',['Row',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Row.html',1,'google::cloud::spanner::v0']]],
   ['rowparser',['RowParser',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1RowParser.html',1,'google::cloud::spanner::v0']]]
 ];
--- latest/search/files_5.js
+++ latest/search/files_5.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['result_5fset_2eh',['result_set.h',['../result__set_8h.html',1,'']]],
   ['retry_5fpolicy_2eh',['retry_policy.h',['../retry__policy_8h.html',1,'']]],
   ['row_2eh',['row.h',['../row_8h.html',1,'']]],
   ['row_5fparser_2eh',['row_parser.h',['../row__parser_8h.html',1,'']]]
