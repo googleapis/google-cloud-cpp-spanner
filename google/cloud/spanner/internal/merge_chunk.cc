7b51b3a6b17d585aaac52ba332744a73192cf3b0
Greg Miller <9447643+devjgm@users.noreply.github.com>
2019-07-22 13:08:00 -0400

    working

--- google/cloud/spanner/internal/merge_chunk.cc
+++ google/cloud/spanner/internal/merge_chunk.cc
@@ -20,6 +20,72 @@ namespace spanner {
 inline namespace SPANNER_CLIENT_NS {
 namespace internal {
 
+Status MergeChunk(google::protobuf::Value& value,
+                  google::protobuf::Value&& chunk) {
+  switch (value.kind_case()) {
+    case google::protobuf::Value::kBoolValue:
+    case google::protobuf::Value::kNumberValue:
+    case google::protobuf::Value::kNullValue:
+      return Status(StatusCode::kInvalidArgument,
+                    "Cannot merge values of type bool/number/null");
+
+    case google::protobuf::Value::kStringValue: {
+      if (chunk.kind_case() != google::protobuf::Value::kStringValue) {
+        return Status(StatusCode::kInvalidArgument,
+                      "Cannot merge mismatched types");
+      }
+      *value.mutable_string_value() += chunk.string_value();
+      return Status();
+    }
+
+    case google::protobuf::Value::kStructValue: {
+      if (chunk.kind_case() != google::protobuf::Value::kStructValue) {
+        return Status(StatusCode::kInvalidArgument,
+                      "Cannot merge mismatched types");
+      }
+      return Status(StatusCode::kInternal,
+                    "Spanner should never return struct_value");
+    }
+
+    case google::protobuf::Value::kListValue: {
+      if (chunk.kind_case() != google::protobuf::Value::kListValue) {
+        return Status(StatusCode::kInvalidArgument,
+                      "Cannot merge mismatched types");
+      }
+
+      auto& value_list = *value.mutable_list_value()->mutable_values();
+      auto& chunk_list = *chunk.mutable_list_value()->mutable_values();
+
+      // Check if we need to recursively merge the last element in value_list
+      // with the first element in chunk_list.
+      if (!value_list.empty()) {
+        auto& last = value_list[value_list.size() - 1];
+        if (last.kind_case() == google::protobuf::Value::kStringValue ||
+            last.kind_case() == google::protobuf::Value::kListValue) {
+          if (chunk_list.empty()) {
+            return Status(StatusCode::kInternal,
+                          "Cannot merge with empty chunk");
+          }
+          auto& first = chunk_list[0];
+          auto const status = MergeChunk(last, std::move(first));
+          if (!status.ok()) return status;
+          chunk_list.erase(chunk_list.begin());
+        }
+      }
+      // Moves all the remaining elements over.
+      for (auto& e : chunk_list) {
+        *value.mutable_list_value()->add_values() = std::move(e);
+      }
+
+      return Status();
+    }
+
+    default:
+      return Status(StatusCode::kUnknown, "Unknown Value type");
+  }
+  return Status(StatusCode::kUnknown, "Unknown Value type");
+}
+
 }  // namespace internal
 }  // namespace SPANNER_CLIENT_NS
 }  // namespace spanner
--- google/cloud/spanner/internal/merge_chunk_test.cc
+++ google/cloud/spanner/internal/merge_chunk_test.cc
@@ -13,6 +13,11 @@
 // limitations under the License.
 
 #include "google/cloud/spanner/internal/merge_chunk.h"
+#include "google/cloud/spanner/value.h"
+#include "google/cloud/testing_util/assert_ok.h"
+#include <google/protobuf/struct.pb.h>
+#include <google/protobuf/text_format.h>
+#include <google/protobuf/util/message_differencer.h>
 #include <gmock/gmock.h>
 
 namespace google {
@@ -22,8 +27,176 @@ inline namespace SPANNER_CLIENT_NS {
 namespace internal {
 namespace {
 
-TEST(MergeChunk, Foo) {
-  /* FAIL(); */
+//
+// MakeProtoValue() is an overloaded helper function for creating
+// google::protobuf::Value protos from convenient user-supplied arguments.
+//
+
+google::protobuf::Value MakeProtoValue(Value v) {
+  google::protobuf::Value value;
+  std::tie(std::ignore, value) = internal::ToProto(std::move(v));
+  return value;
+}
+
+google::protobuf::Value MakeProtoValue(std::string s) {
+  google::protobuf::Value value;
+  value.set_string_value(std::move(s));
+  return value;
+}
+
+google::protobuf::Value MakeProtoValue(double d) {
+  google::protobuf::Value value;
+  value.set_number_value(d);
+  return value;
+}
+
+template <typename T>
+google::protobuf::Value MakeProtoValue(std::vector<T> v) {
+  google::protobuf::Value value;
+  for (auto& e : v) {
+    *value.mutable_list_value()->add_values() = MakeProtoValue(std::move(e));
+  }
+  return value;
+}
+
+// Example from
+// https://github.com/googleapis/googleapis/blob/master/google/spanner/v1/result_set.proto
+//
+// "foo", "bar" => "foobar"
+TEST(MergeChunk, ExampleStrings) {
+  auto a = MakeProtoValue("foo");
+  auto b = MakeProtoValue("bar");
+  ASSERT_STATUS_OK(MergeChunk(a, std::move(b)));
+
+  auto expected = MakeProtoValue("foobar");
+  std::string delta;
+  google::protobuf::util::MessageDifferencer differencer;
+  differencer.ReportDifferencesToString(&delta);
+  EXPECT_TRUE(differencer.Compare(a, expected)) << delta;
+}
+
+// Example from
+// https://github.com/googleapis/googleapis/blob/master/google/spanner/v1/result_set.proto
+//
+// [2, 3], [4] => [2, 3, 4]
+TEST(MergeChunk, ExampleListOfInts) {
+  auto a = MakeProtoValue(std::vector<double>{2, 3});
+  auto b = MakeProtoValue(std::vector<double>{4});
+  ASSERT_STATUS_OK(MergeChunk(a, std::move(b)));
+
+  auto expected = MakeProtoValue(std::vector<double>{2, 3, 4});
+  std::string delta;
+  google::protobuf::util::MessageDifferencer differencer;
+  differencer.ReportDifferencesToString(&delta);
+  EXPECT_TRUE(differencer.Compare(a, expected)) << delta;
+}
+
+// Example from
+// https://github.com/googleapis/googleapis/blob/master/google/spanner/v1/result_set.proto
+//
+// ["a", "b"], ["c", "d"] => ["a", "bc", "d"]
+TEST(MergeChunk, ExampleListOfStrings) {
+  auto a = MakeProtoValue(std::vector<std::string>{"a", "b"});
+  auto b = MakeProtoValue(std::vector<std::string>{"c", "d"});
+  ASSERT_STATUS_OK(MergeChunk(a, std::move(b)));
+
+  auto expected = MakeProtoValue(std::vector<std::string>{"a", "bc", "d"});
+  std::string delta;
+  google::protobuf::util::MessageDifferencer differencer;
+  differencer.ReportDifferencesToString(&delta);
+  EXPECT_TRUE(differencer.Compare(a, expected)) << delta;
+}
+
+// Example from
+// https://github.com/googleapis/googleapis/blob/master/google/spanner/v1/result_set.proto
+//
+// ["a", ["b", "c"]], [["d"], "e"] => ["a", ["b", "cd"], "e"]
+TEST(MergeChunk, ExampleListsOfListOfString) {
+  auto a = MakeProtoValue(std::vector<Value>{
+      Value("a"), Value(std::vector<std::string>{"b", "c"})});
+  auto b = MakeProtoValue(
+      std::vector<Value>{Value(std::vector<std::string>{"d"}), Value("e")});
+  ASSERT_STATUS_OK(MergeChunk(a, std::move(b)));
+
+  auto expected = MakeProtoValue(std::vector<Value>{
+      Value("a"), Value(std::vector<std::string>{"b", "cd"}), Value("e")});
+
+  std::string delta;
+  google::protobuf::util::MessageDifferencer differencer;
+  differencer.ReportDifferencesToString(&delta);
+  EXPECT_TRUE(differencer.Compare(a, expected)) << delta;
+}
+
+//
+// Tests some edge cases that we think should probably work.
+//
+
+TEST(MergeChunk, EmptyString) {
+  auto value = MakeProtoValue("foo");
+  ASSERT_STATUS_OK(MergeChunk(value, MakeProtoValue("")));
+
+  std::string delta;
+  google::protobuf::util::MessageDifferencer differencer;
+  differencer.ReportDifferencesToString(&delta);
+  EXPECT_TRUE(differencer.Compare(value, MakeProtoValue("foo"))) << delta;
+
+  auto empty = MakeProtoValue("");
+  ASSERT_STATUS_OK(MergeChunk(empty, MakeProtoValue("bar")));
+
+  delta.clear();
+  EXPECT_TRUE(differencer.Compare(empty, MakeProtoValue("bar"))) << delta;
+}
+
+//
+// Tests the unsupported cases
+//
+
+TEST(MergeChunk, CannotMergeBools) {
+  google::protobuf::Value bool1;
+  bool1.set_bool_value(true);
+
+  google::protobuf::Value bool2;
+  bool2.set_bool_value(true);
+
+  auto status = MergeChunk(bool1, std::move(bool2));
+  EXPECT_FALSE(status.ok());
+  EXPECT_THAT(status.message(), testing::HasSubstr("Cannot merge"));
+}
+
+TEST(MergeChunk, CannotMergeNumbers) {
+  google::protobuf::Value number1;
+  number1.set_number_value(1.0);
+
+  google::protobuf::Value number2;
+  number2.set_number_value(2.0);
+
+  auto status = MergeChunk(number1, std::move(number2));
+  EXPECT_FALSE(status.ok());
+  EXPECT_THAT(status.message(), testing::HasSubstr("Cannot merge"));
+}
+
+TEST(MergeChunk, CannotMergeNull) {
+  google::protobuf::Value null1;
+  null1.set_null_value(google::protobuf::NullValue::NULL_VALUE);
+
+  google::protobuf::Value null2;
+  null2.set_null_value(google::protobuf::NullValue::NULL_VALUE);
+
+  auto status = MergeChunk(null1, std::move(null2));
+  EXPECT_FALSE(status.ok());
+  EXPECT_THAT(status.message(), testing::HasSubstr("Cannot merge"));
+}
+
+TEST(MergeChunk, CannotMergeStruct) {
+  google::protobuf::Value struct1;
+  struct1.mutable_struct_value();
+
+  google::protobuf::Value struct2;
+  struct2.mutable_struct_value();
+
+  auto status = MergeChunk(struct1, std::move(struct2));
+  EXPECT_FALSE(status.ok());
+  EXPECT_THAT(status.message(), testing::HasSubstr("Spanner should never"));
 }
 
 }  // namespace
