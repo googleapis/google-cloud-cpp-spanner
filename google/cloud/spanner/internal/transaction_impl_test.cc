36ce564a0e55077fde9c1f8b9e90c8c72103696a
Greg Miller <9447643+devjgm@users.noreply.github.com>
2019-07-27 13:53:58 -0700

    pr comments

--- google/cloud/spanner/internal/transaction_impl_test.cc
+++ google/cloud/spanner/internal/transaction_impl_test.cc
@@ -121,12 +121,11 @@ ResultSet Client::Read(TransactionSelector& selector, std::string const&,
         selector.set_id(txn_id_);
         break;
       case Mode::kReadFails:  // leave as `begin`, calls stay serialized
+        if (fail_with_throw) {
 #if __EXCEPTIONS
-        if (fail_with_throw) throw "1202 Program Alarm";
+          throw "1202 Program Alarm";
 #endif
-        // Quiets a compiler warning about an unused variable (when exceptions
-        // are disabled).
-        (void)fail_with_throw;
+        }
         break;
     }
   } else {
