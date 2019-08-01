364db20ade5888f9c6cb2537a5eeb113a364f1d1
Greg Miller <9447643+devjgm@users.noreply.github.com>
2019-08-01 11:12:13 -0400

    added scripts to make bazel build look like cmake build

---
+++ bazel/compiler_id.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Given an argument that is the path to a compiler, tries to determine an
+# identification string for the compiler according to the CMake-defined IDs:
+# https://cmake.org/cmake/help/v3.5/variable/CMAKE_LANG_COMPILER_ID.html
+
+if [[ ! -e "${1}" ]]; then
+  echo "Usage: $0 <path-to-compiler>"
+  exit 1
+fi
+
+version="$(${1} --version 2> /dev/null \
+  | head -1 \
+  | tr '[:upper:]' '[:lower:]')"
+
+case "${version}" in
+  *gcc* | *g++*)
+    echo "GNU"
+    ;;
+
+  *clang*)
+    echo "Clang"
+    ;;
+
+  *msvc* | *microsoft*)
+    echo "Clang"
+    ;;
+
+  *)
+    echo "unknown"
+    ;;
+esac
