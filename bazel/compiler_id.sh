2bf41cb88114d046dbb60a115496780e40f5810b
Greg Miller <9447643+devjgm@users.noreply.github.com>
2019-08-02 07:58:19 -0400

    changed usage-check in scripts

--- bazel/compiler_id.sh
+++ bazel/compiler_id.sh
@@ -18,7 +18,7 @@
 # identification string for the compiler according to the CMake-defined IDs:
 # https://cmake.org/cmake/help/v3.5/variable/CMAKE_LANG_COMPILER_ID.html
 
-if [[ ! -e "${1}" ]]; then
+if [[ $# -ne 1 ]]; then
   echo "Usage: $0 <path-to-compiler>"
   exit 1
 fi
--- bazel/compiler_version.sh
+++ bazel/compiler_version.sh
@@ -18,9 +18,17 @@
 # the compiler's version by looking for the first string that looks like
 # X.Y with more optional numbers, '.', '-', and '+'.
 
-if [[ ! -e "${1}" ]]; then
+if [[ $# -ne 1 ]]; then
   echo "Usage: $0 <path-to-compiler>"
   exit 1
 fi
 
-"${1}" --version 2> /dev/null | grep -Eo "[0-9]+\.[0-9.+-]+" | head -1
+version="$("${1}" --version 2> /dev/null \
+  | grep -Eo "[0-9]+\.[0-9.+-]+" \
+  | head -1)"
+
+if [[ -n "${version}" ]]; then
+  echo "${version}"
+else
+  echo unknown
+fi
