9e466301e32fc40f8b53d4b5c224cf9a8a7de581
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-08-02 15:59:43 -0700

    Automatically generated documentation

--- latest/search/functions_3.js
+++ latest/search/functions_3.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['databaseadminclient',['DatabaseAdminClient',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminClient.html#abdd9620e892e6be196c2abc1840eb40f',1,'google::cloud::spanner::v0::DatabaseAdminClient::DatabaseAdminClient(ClientOptions const &amp;client_options=ClientOptions())'],['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminClient.html#a6c97182b1d2d562a7f09c42599e31275',1,'google::cloud::spanner::v0::DatabaseAdminClient::DatabaseAdminClient(std::shared_ptr&lt; internal::DatabaseAdminStub &gt; s)']]],
-  ['date',['Date',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Date.html#aee96fb4664e22cad5a1927aade88e43a',1,'google::cloud::spanner::v0::Date::Date()'],['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Date.html#a1c3ace1b2fbf93fcbdc20e234d9ffabb',1,'google::cloud::spanner::v0::Date::Date(std::int64_t year, int month, int day)']]],
+  ['date',['Date',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Date.html#a1c3ace1b2fbf93fcbdc20e234d9ffabb',1,'google::cloud::spanner::v0::Date::Date(std::int64_t year, int month, int day)'],['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Date.html#aee96fb4664e22cad5a1927aade88e43a',1,'google::cloud::spanner::v0::Date::Date()']]],
   ['day',['day',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Date.html#a45aa5e22c824b5ad02b81ccc58efc4ae',1,'google::cloud::spanner::v0::Date']]],
   ['deserializesqlpartition',['DeserializeSqlPartition',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#aa0a97e4a5530bb2ddb8a26e4ed4998d0',1,'google::cloud::spanner::v0']]],
   ['dropdatabase',['DropDatabase',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminClient.html#a91faa307a79abe7867622e310d249ad9',1,'google::cloud::spanner::v0::DatabaseAdminClient']]]
