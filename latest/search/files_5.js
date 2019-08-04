edb441933e9c7365842bbb9e9d8455a3763b6e6a
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-08-04 16:09:04 -0700

    Automatically generated documentation

--- latest/search/files_5.js
+++ latest/search/files_5.js
@@ -1,5 +1,7 @@
 var searchData=
 [
+  ['random_5fdatabase_5fname_2ecc',['random_database_name.cc',['../random__database__name_8cc.html',1,'']]],
+  ['random_5fdatabase_5fname_2eh',['random_database_name.h',['../random__database__name_8h.html',1,'']]],
   ['result_5fset_2eh',['result_set.h',['../result__set_8h.html',1,'']]],
   ['retry_5fpolicy_2eh',['retry_policy.h',['../retry__policy_8h.html',1,'']]],
   ['row_2eh',['row.h',['../row_8h.html',1,'']]],
