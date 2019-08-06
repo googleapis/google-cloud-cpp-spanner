7a3c045d586174c6d3c5a8c4d44be05267a59e2c
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-08-06 05:40:09 -0700

    Automatically generated documentation

--- latest/search/all_2.js
+++ latest/search/all_2.js
@@ -7,7 +7,7 @@ var searchData=
   ['client_5foptions_2eh',['client_options.h',['../client__options_8h.html',1,'']]],
   ['clientoptions',['ClientOptions',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1ClientOptions.html',1,'google::cloud::spanner::v0::ClientOptions'],['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1ClientOptions.html#ae97202f5b605eb64cdafd36d0b711e43',1,'google::cloud::spanner::v0::ClientOptions::ClientOptions()'],['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1ClientOptions.html#a1ef5cafb8727405a0c4c76ad9c4feeb1',1,'google::cloud::spanner::v0::ClientOptions::ClientOptions(std::shared_ptr&lt; grpc::ChannelCredentials &gt; c)']]],
   ['clone',['clone',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1PollingPolicy.html#a84ec286d5ba523c4c9816dc95efc4ff3',1,'google::cloud::spanner::v0::PollingPolicy::clone()'],['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1GenericPollingPolicy.html#a34768dd99b02713cbc62d4e0608d045e',1,'google::cloud::spanner::v0::GenericPollingPolicy::clone()']]],
-  ['commit',['Commit',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Client.html#a99b8b8ec5092e7fe35059add0ea7c5ed',1,'google::cloud::spanner::v0::Client']]],
+  ['commit',['Commit',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Client.html#ade201971b6a4f026c65ead7cf9124fd5',1,'google::cloud::spanner::v0::Client']]],
   ['commit_5fresult_2eh',['commit_result.h',['../commit__result_8h.html',1,'']]],
   ['commit_5ftimestamp',['commit_timestamp',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1CommitResult.html#a66fb3611f13e6fba76c31e8680d90419',1,'google::cloud::spanner::v0::CommitResult']]],
   ['commitresult',['CommitResult',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1CommitResult.html',1,'google::cloud::spanner::v0']]],
--- latest/search/functions_0.js
+++ latest/search/functions_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['admin_5fendpoint',['admin_endpoint',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1ClientOptions.html#a0e0d1ac7f3559bfe76491439e6b90f79',1,'google::cloud::spanner::v0::ClientOptions']]],
-  ['as_5fproto',['as_proto',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Mutation.html#ab4ae1acc2f396d8dd412315c05eb79bb',1,'google::cloud::spanner::v0::Mutation']]]
+  ['as_5fproto',['as_proto',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Mutation.html#ab4ae1acc2f396d8dd412315c05eb79bb',1,'google::cloud::spanner::v0::Mutation::as_proto() &amp;&amp;'],['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Mutation.html#ade4a1b0f4e04862b8e6c8d4bea2265b0',1,'google::cloud::spanner::v0::Mutation::as_proto() const &amp;']]]
 ];
