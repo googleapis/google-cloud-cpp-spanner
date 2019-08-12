982c2c9c6a51dbc85dcfff566fabe3b089e60316
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-08-12 11:56:52 -0700

    Automatically generated documentation

--- latest/search/classes_8.js
+++ latest/search/classes_8.js
@@ -4,6 +4,7 @@ var searchData=
   ['readoptions',['ReadOptions',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1ReadOptions.html',1,'google::cloud::spanner::v0']]],
   ['readwriteoptions',['ReadWriteOptions',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Transaction_1_1ReadWriteOptions.html',1,'google::cloud::spanner::v0::Transaction']]],
   ['resultset',['ResultSet',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1ResultSet.html',1,'google::cloud::spanner::v0']]],
+  ['rollbackparams',['RollbackParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1Connection_1_1RollbackParams.html',1,'google::cloud::spanner::v0::Connection']]],
   ['row',['Row',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Row.html',1,'google::cloud::spanner::v0']]],
   ['rowparser',['RowParser',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1RowParser.html',1,'google::cloud::spanner::v0']]]
 ];
--- latest/search/variables_5.js
+++ latest/search/variables_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['transaction',['transaction',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1Connection_1_1CommitParams.html#a60650945ef9fb0dd61545b9ac30e33f6',1,'google::cloud::spanner::v0::Connection::CommitParams']]]
+  ['transaction',['transaction',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1Connection_1_1CommitParams.html#a60650945ef9fb0dd61545b9ac30e33f6',1,'google::cloud::spanner::v0::Connection::CommitParams::transaction()'],['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1Connection_1_1RollbackParams.html#a54a867b715f9a9aae9dfe35e1dca6d93',1,'google::cloud::spanner::v0::Connection::RollbackParams::transaction()']]]
 ];
