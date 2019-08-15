fde28a49b850ac1018afa6f15bfbc20debae6d6e
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-08-15 10:02:52 -0700

    Automatically generated documentation

--- latest/search/all_6.js
+++ latest/search/all_6.js
@@ -3,7 +3,6 @@ var searchData=
   ['index_5fname',['index_name',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1ReadOptions.html#a222f955bec24a88a1f2ac7931046075b',1,'google::cloud::spanner::v0::ReadOptions']]],
   ['insertmutationbuilder',['InsertMutationBuilder',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#a7447c52a5348dfc769538a160c0d455a',1,'google::cloud::spanner::v0']]],
   ['insertorupdatemutationbuilder',['InsertOrUpdateMutationBuilder',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#ac6688b3bbd87a81055018d61f37fd942',1,'google::cloud::spanner::v0']]],
-  ['isall',['IsAll',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1KeySet.html#a603f737eb1e6ddb878a9c5023d65c33b',1,'google::cloud::spanner::v0::KeySet']]],
   ['isclosed',['IsClosed',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Bound.html#a3784548fb355027a49227024250a4d8e',1,'google::cloud::spanner::v0::Bound']]],
   ['isopen',['IsOpen',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Bound.html#a8dd95195d4708ad00787ffbeeb2dbaa3',1,'google::cloud::spanner::v0::Bound']]],
   ['iterator',['iterator',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1RowParser_1_1iterator.html',1,'google::cloud::spanner::v0::RowParser']]],
--- latest/search/functions_6.js
+++ latest/search/functions_6.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['isall',['IsAll',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1KeySet.html#a603f737eb1e6ddb878a9c5023d65c33b',1,'google::cloud::spanner::v0::KeySet']]],
   ['isclosed',['IsClosed',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Bound.html#a3784548fb355027a49227024250a4d8e',1,'google::cloud::spanner::v0::Bound']]],
   ['isopen',['IsOpen',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Bound.html#a8dd95195d4708ad00787ffbeeb2dbaa3',1,'google::cloud::spanner::v0::Bound']]]
 ];
