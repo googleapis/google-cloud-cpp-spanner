6842d2260fae8b617b014bcc4d4db27ea27817f4
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-08-15 12:33:52 -0700

    Automatically generated documentation

--- latest/search/functions_12.js
+++ latest/search/functions_12.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['_7econnection',['~Connection',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Connection.html#a607c0d58ee08ad7bf31a4ad82eb6782d',1,'google::cloud::spanner::v0::Connection']]],
-  ['_7ekeyrange',['~KeyRange',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1KeyRange.html#a22ed1e402bafe1b93c1fba54f409e04d',1,'google::cloud::spanner::v0::KeyRange']]],
   ['_7epollingpolicy',['~PollingPolicy',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1PollingPolicy.html#afd75230907ff017e9cc6def345700840',1,'google::cloud::spanner::v0::PollingPolicy']]],
   ['_7etransaction',['~Transaction',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Transaction.html#a7e319af8d1f818ded6d6b7385de5e9f7',1,'google::cloud::spanner::v0::Transaction']]]
 ];
