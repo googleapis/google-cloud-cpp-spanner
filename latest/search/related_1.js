0b550a59d13a29595b8881b99fcd1795d401cb4a
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-08-15 07:28:27 -0700

    Automatically generated documentation

--- latest/search/related_1.js
+++ latest/search/related_1.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['makeboundclosed',['MakeBoundClosed',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Bound.html#aab0f4590ad9b4069ecfd6c2112032c56',1,'google::cloud::spanner::v0::Bound']]],
-  ['makeboundopen',['MakeBoundOpen',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Bound.html#ad3dcc1aeb24e952a641235b460891c23',1,'google::cloud::spanner::v0::Bound']]],
-  ['makekeyrange',['MakeKeyRange',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Bound.html#a7cd56ae47231fc790b97af22b104307c',1,'google::cloud::spanner::v0::Bound']]]
+  ['makeboundopen',['MakeBoundOpen',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Bound.html#ad3dcc1aeb24e952a641235b460891c23',1,'google::cloud::spanner::v0::Bound']]]
 ];
