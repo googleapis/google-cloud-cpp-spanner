140838642e54fa1966342e0beb0e88a6a4a9a1ba
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-08-16 07:17:34 -0700

    Automatically generated documentation

--- latest/search/related_3.js
+++ latest/search/related_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['printto',['PrintTo',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Mutation.html#a83923150406f4df48162a621a6774f6d',1,'google::cloud::spanner::v0::Mutation::PrintTo()'],['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Value.html#a0a7a7c7991836200e205946a7abceed1',1,'google::cloud::spanner::v0::Value::PrintTo()']]]
+  ['printto',['PrintTo',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Mutation.html#a83923150406f4df48162a621a6774f6d',1,'google::cloud::spanner::v0::Mutation::PrintTo()'],['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Row.html#a4e4e2c944d8801bda60b4814b9c6f7b4',1,'google::cloud::spanner::v0::Row::PrintTo()'],['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Value.html#a0a7a7c7991836200e205946a7abceed1',1,'google::cloud::spanner::v0::Value::PrintTo()']]]
 ];
