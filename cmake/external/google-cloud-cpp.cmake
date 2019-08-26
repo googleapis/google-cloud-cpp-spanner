33fd5e65cfe18506254fdfc457abc0f8bb207ee4
Carlos O'Ryan <coryan@google.com>
2019-08-26 09:18:17 -0400

    chore: Update googleapis version.

--- cmake/external/google-cloud-cpp.cmake
+++ cmake/external/google-cloud-cpp.cmake
@@ -19,22 +19,17 @@ include(external/external-project-helpers)
 include(external/curl)
 include(external/crc32c)
 include(external/grpc)
+include(external/googleapis)
 
 if (NOT TARGET google-cloud-cpp-project)
     # Give application developers a hook to configure the version and hash
     # downloaded from GitHub.
     set(
         GOOGLE_CLOUD_CPP_URL
-        "https://github.com/googleapis/google-cloud-cpp/archive/2de685053a3ba44afb43531776e0eaada3c35e4c.tar.gz"
+        "https://github.com/googleapis/google-cloud-cpp/archive/92f412c4e5dcb02924120cb170c4f6b0e90be290.tar.gz"
         )
     set(GOOGLE_CLOUD_CPP_SHA256
-        "f80a3eb3b3f265b226386929d4b0547ba8b0c00534d65e220518b81b28f4feee")
-    set(
-        GOOGLEAPIS_URL
-        "https://github.com/google/googleapis/archive/a8ee1416f4c588f2ab92da72e7c1f588c784d3e6.zip"
-        )
-    set(GOOGLEAPIS_SHA56
-        "6b8a9b2bcb4476e9a5a9872869996f0d639c8d5df76dd8a893e79201f211b1cf")
+        "82c57760e0c883e6cdd9f023f8445e50c5eacf818aea0b616ccb1fa5257e8f05")
 
     google_cloud_cpp_set_prefix_vars()
 
@@ -42,7 +37,10 @@ if (NOT TARGET google-cloud-cpp-project)
 
     ExternalProject_Add(
         google-cloud-cpp-project
-        DEPENDS grpc-project curl-project crc32c-project
+        DEPENDS googleapis-project
+                grpc-project
+                curl-project
+                crc32c-project
         EXCLUDE_FROM_ALL ON
         PREFIX "${CMAKE_BINARY_DIR}/external/google-cloud-cpp"
         INSTALL_DIR "${GOOGLE_CLOUD_CPP_EXTERNAL_PREFIX}"
@@ -50,9 +48,6 @@ if (NOT TARGET google-cloud-cpp-project)
         URL_HASH SHA256=${GOOGLE_CLOUD_CPP_SHA256}
         LIST_SEPARATOR |
         CMAKE_ARGS -DBUILD_TESTING=OFF
-                   -DGOOGLE_CLOUD_CPP_DEPENDENCY_PROVIDER=package
-                   -DGOOGLE_CLOUD_CPP_GOOGLEAPIS_URL=${GOOGLEAPIS_URL}
-                   -DGOOGLE_CLOUD_CPP_GOOGLEAPIS_SHA256=${GOOGLEAPIS_SHA256}
                    -DGOOGLE_CLOUD_CPP_TESTING_UTIL_ENABLE_INSTALL=ON
                    -DCMAKE_PREFIX_PATH=${GOOGLE_CLOUD_CPP_PREFIX_PATH}
                    -DCMAKE_INSTALL_RPATH=${GOOGLE_CLOUD_CPP_INSTALL_RPATH}
