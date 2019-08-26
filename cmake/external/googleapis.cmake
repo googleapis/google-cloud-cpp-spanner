113fe6d01ba5ed5fc98a25b3a3d8704c2f0504ec
Carlos O'Ryan <coryan@users.noreply.github.com>
2019-08-26 12:28:23 -0400

    chore: Update googleapis version. (#438)

--- cmake/external/googleapis.cmake
+++ cmake/external/googleapis.cmake
@@ -15,41 +15,37 @@
 # ~~~
 
 find_package(Threads REQUIRED)
-include(ExternalProject)
 include(external/external-project-helpers)
-include(external/zlib)
 
-if (NOT TARGET protobuf-project)
+include(external/grpc)
+
+if (NOT TARGET googleapis-project)
     # Give application developers a hook to configure the version and hash
     # downloaded from GitHub.
-    set(GOOGLE_CLOUD_CPP_PROTOBUF_URL
-        "https://github.com/google/protobuf/archive/v3.7.1.tar.gz")
-    set(GOOGLE_CLOUD_CPP_PROTOBUF_SHA256
-        "f1748989842b46fa208b2a6e4e2785133cfcc3e4d43c17fecb023733f0f5443f")
+    set(GOOGLE_CLOUD_CPP_GOOGLEAPIS_URL
+        "https://github.com/googleapis/cpp-cmakefiles/archive/v0.1.3.tar.gz")
+    set(GOOGLE_CLOUD_CPP_GOOGLEAPIS_SHA256
+        "8abbefe4818070b0b7cc15dcfe72a8c316f39312eec53bb478f55b077beb5c20")
 
     google_cloud_cpp_set_prefix_vars()
 
     set_external_project_build_parallel_level(PARALLEL)
 
+    include(ExternalProject)
     ExternalProject_Add(
-        protobuf-project
-        DEPENDS zlib-project
+        googleapis-project
+        DEPENDS grpc-project
         EXCLUDE_FROM_ALL ON
-        PREFIX "${CMAKE_BINARY_DIR}/external/protobuf"
+        PREFIX "${CMAKE_BINARY_DIR}/external/googleapis"
         INSTALL_DIR "${GOOGLE_CLOUD_CPP_EXTERNAL_PREFIX}"
-        URL ${GOOGLE_CLOUD_CPP_PROTOBUF_URL}
-        URL_HASH SHA256=${GOOGLE_CLOUD_CPP_PROTOBUF_SHA256}
+        URL ${GOOGLE_CLOUD_CPP_GOOGLEAPIS_URL}
+        URL_HASH SHA256=${GOOGLE_CLOUD_CPP_GOOGLEAPIS_SHA256}
         LIST_SEPARATOR |
-        CONFIGURE_COMMAND
-            ${CMAKE_COMMAND}
-            -G${CMAKE_GENERATOR}
-            -Dprotobuf_BUILD_TESTS=OFF
-            -Dprotobuf_DEBUG_POSTFIX=
-            -H<SOURCE_DIR>/cmake
-            -B<BINARY_DIR>
-            -DCMAKE_PREFIX_PATH=${GOOGLE_CLOUD_CPP_PREFIX_PATH}
-            -DCMAKE_INSTALL_RPATH=${GOOGLE_CLOUD_CPP_INSTALL_RPATH}
-            -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+        CMAKE_ARGS -G${CMAKE_GENERATOR}
+                   -DCMAKE_PREFIX_PATH=${GOOGLE_CLOUD_CPP_PREFIX_PATH}
+                   -DCMAKE_INSTALL_RPATH=${GOOGLE_CLOUD_CPP_INSTALL_RPATH}
+                   -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}
+                   -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
         BUILD_COMMAND ${CMAKE_COMMAND}
                       --build
                       <BINARY_DIR>
