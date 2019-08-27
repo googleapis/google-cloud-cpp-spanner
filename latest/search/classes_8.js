40d4033c80c5d74b519ca3c7d8febdbb38166e3e
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-08-27 11:55:06 -0700

    Automatically generated documentation

--- latest/search/classes_8.js
+++ latest/search/classes_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['partitionqueryparams',['PartitionQueryParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1Connection_1_1PartitionQueryParams.html',1,'google::cloud::spanner::v0::Connection']]],
   ['partitionreadparams',['PartitionReadParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1Connection_1_1PartitionReadParams.html',1,'google::cloud::spanner::v0::Connection']]],
   ['pollingpolicy',['PollingPolicy',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1PollingPolicy.html',1,'google::cloud::spanner::v0']]]
 ];
--- latest/search/functions_b.js
+++ latest/search/functions_b.js
@@ -4,7 +4,7 @@ var searchData=
   ['params',['params',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1SqlStatement.html#a1f2f5bda056f807a3f5410a0b10c5f5c',1,'google::cloud::spanner::v0::SqlStatement']]],
   ['parentname',['ParentName',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Database.html#a894e8ab0fd1214e84ff44df1c4f8179c',1,'google::cloud::spanner::v0::Database']]],
   ['parserow',['ParseRow',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#a6a6319e9526591fe4496f5e0876c99ec',1,'google::cloud::spanner::v0']]],
-  ['partitionquery',['PartitionQuery',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Client.html#a80dafb769a6a297cdf1e6ea1e03d5131',1,'google::cloud::spanner::v0::Client']]],
+  ['partitionquery',['PartitionQuery',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Client.html#a501a9d95abac856b077d1e82c11c4135',1,'google::cloud::spanner::v0::Client::PartitionQuery()'],['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Connection.html#a692dd4b55392f2b1146c903ffdea9538',1,'google::cloud::spanner::v0::Connection::PartitionQuery()']]],
   ['partitionread',['PartitionRead',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Client.html#ae79520d087ff55c903e2351c5d36a446',1,'google::cloud::spanner::v0::Client::PartitionRead()'],['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Connection.html#a1454df6f87dde5f097df21e2e26d607d',1,'google::cloud::spanner::v0::Connection::PartitionRead()']]],
   ['printto',['PrintTo',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#a84d22bc4420b8d5ef47776274fce29ee',1,'google::cloud::spanner::v0::PrintTo(Mutation const &amp;m, std::ostream *os)'],['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#afeeb8397799463d7cf6386cd1315013e',1,'google::cloud::spanner::v0::PrintTo(Value const &amp;v, std::ostream *os)']]]
 ];
