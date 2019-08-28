25cd01ca7f2bef29d93efb70f8d44825132e03db
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-08-28 13:32:51 -0700

    Automatically generated documentation

--- latest/search/variables_8.js
+++ latest/search/variables_8.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['session_5fname',['session_name',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1Connection_1_1ReadParams.html#a0d6619b4783d35a0c342b02561ae4cd4',1,'google::cloud::spanner::v0::Connection::ReadParams::session_name()'],['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1Connection_1_1ExecuteSqlParams.html#aae495e48882e13b89663668388a55097',1,'google::cloud::spanner::v0::Connection::ExecuteSqlParams::session_name()']]],
   ['sql_5fparams',['sql_params',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1Connection_1_1PartitionQueryParams.html#a80b518edf98c55179a53ff8c1645e41d',1,'google::cloud::spanner::v0::Connection::PartitionQueryParams']]],
   ['statement',['statement',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1Connection_1_1ExecuteSqlParams.html#a62bbdf511297e5c246237b89395e4aa4',1,'google::cloud::spanner::v0::Connection::ExecuteSqlParams']]]
 ];
