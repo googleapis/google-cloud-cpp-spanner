07920694b6bc2042286c7c28e7c5541db4d788e0
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-09-04 16:15:02 -0700

    Automatically generated documentation

--- latest/search/functions_10.js
+++ latest/search/functions_10.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['updatedatabase',['UpdateDatabase',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminClient.html#a806002777837f8f408848e5cbf2238c1',1,'google::cloud::spanner::v0::DatabaseAdminClient']]],
-  ['user_5fagent_5fprefix',['user_agent_prefix',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1ConnectionOptions.html#a2d283cc38119f2d56c6c2ec2ae7a5247',1,'google::cloud::spanner::v0::ConnectionOptions']]]
+  ['updatedatabase',['UpdateDatabase',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminClient.html#a806002777837f8f408848e5cbf2238c1',1,'google::cloud::spanner::v0::DatabaseAdminClient']]]
 ];
