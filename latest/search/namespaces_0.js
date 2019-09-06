9d9918297cce553a9d8a0cb7d03029b439e6aa46
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-09-06 11:59:50 -0700

    Automatically generated documentation

--- latest/search/namespaces_0.js
+++ latest/search/namespaces_0.js
@@ -3,6 +3,7 @@ var searchData=
   ['cloud',['cloud',['../namespacegoogle_1_1cloud.html',1,'google']]],
   ['google',['google',['../namespacegoogle.html',1,'']]],
   ['spanner',['spanner',['../namespacegoogle_1_1cloud_1_1spanner.html',1,'google::cloud']]],
+  ['spanner_5fmocks',['spanner_mocks',['../namespacegoogle_1_1cloud_1_1spanner__mocks.html',1,'google::cloud']]],
   ['spanner_5ftesting',['spanner_testing',['../namespacegoogle_1_1cloud_1_1spanner__testing.html',1,'google::cloud']]],
-  ['v0',['v0',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html',1,'google::cloud::spanner::v0'],['../namespacegoogle_1_1cloud_1_1spanner__testing_1_1v0.html',1,'google::cloud::spanner_testing::v0']]]
+  ['v0',['v0',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html',1,'google::cloud::spanner::v0'],['../namespacegoogle_1_1cloud_1_1spanner__mocks_1_1v0.html',1,'google::cloud::spanner_mocks::v0'],['../namespacegoogle_1_1cloud_1_1spanner__testing_1_1v0.html',1,'google::cloud::spanner_testing::v0']]]
 ];
