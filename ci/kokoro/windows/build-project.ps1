9b1788afe70abb76a5ae0f627656af105db70532
Takashi Matsuo <tmatsuo@google.com>
2019-09-10 12:07:03 -0700

    test: Build files for Windows CMake (#595)
    * Add files for Windows CMake build
    
    * Avoid warning
    
    * Only remove filed when KOKORO_ARTIFACTS_DIR is set
    
    * Add using testing::DoAll
    
    * Avoid secure warnings on Windows
    
    * Install testing library
    
    * Do not build tests for google-cloud-cpp
    
    * Add CMAKE_CXX_FLAGS
    
    * Avoid secure warnings on Windows
    
    * Avoid conversion warning on Windows
    
    * Set values for integration tests
    
    * Avoid conversion warning on Windows
    
    * Add more environment variable for tests
    
    * Add another environment variable for tests
    
    * Run tests serially
    
    * Exclude integration tests
    
    * Use the correct directory
    
    * Fix for clang-tidy
    
    * Change ExtractTupleValues::i to int
    
    * Set _CRT_SECURE_NO_WARNINGS in CMake file

---
+++ ci/kokoro/windows/build-project.ps1
@@ -0,0 +1,71 @@
+#!/usr/bin/env powershell
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Stop on errors. This is similar to `set -e` on Unix shells.
+$ErrorActionPreference = "Stop"
+
+Write-Host "================================================================"
+Write-Host "Install Pscx module for current user $(Get-Date -Format o)."
+Install-Module Pscx -Force -AllowClobber -Scope CurrentUser
+if ($LastExitCode) {
+    throw "Error installing Pscx module $LastExitCode"
+}
+
+Write-Host "================================================================"
+Write-Host "Run CMake to create build scripts $(Get-Date -Format o)."
+
+# This script expects vcpkg to be installed in ..\vcpkg, discover the full
+# path to that directory:
+$dir = Split-Path (Get-Item -Path ".\" -Verbose).FullName
+
+$cmake_flags=@(
+    # On Windows ninja is a much faster generator.
+    "-GNinja",
+    "-Hci/kokoro/windows",
+    # Use short path names because we only have 255 characters.
+    "-Bcmake-out/w",
+    # We need to compile OpenSSL using vcpkg because compiling from scratch is hard.
+    "-DCMAKE_TOOLCHAIN_FILE=`"$dir\vcpkg\scripts\buildsystems\vcpkg.cmake`"",
+    "-DVCPKG_TARGET_TRIPLET=x64-windows-static",
+    "-DCMAKE_PREFIX_PATH=c:/build/vcpkg/installed/x64-windows-static",
+    # Explicitly set the compiler, otherwise CMake picks up CLang or GCC if installed.
+    "-DCMAKE_C_COMPILER=cl.exe",
+    "-DCMAKE_CXX_COMPILER=cl.exe",
+    "-DCMAKE_BUILD_TYPE=Release"
+)
+# Configure CMake and create the build directory.
+Write-Host "================================================================"
+Write-Host "Configuring CMake with ${cmake_flags} $(Get-Date -Format o)"
+cmake ${cmake_flags}
+if ($LastExitCode) {
+    throw "cmake config failed with exit code $LastExitCode"
+}
+
+Write-Host "================================================================"
+Write-Host "Compiling the project $(Get-Date -Format o)."
+cmake --build cmake-out/w
+if ($LastExitCode) {
+    throw "cmake failed with exit code $LastExitCode"
+}
+
+if (Test-Path env:RUN_INTEGRATION_TESTS ) {
+  Write-Host "================================================================"
+  Write-Host "Run integration tests $(Get-Date -Format o)."
+  cd cmake-out\w\build\g-c-spanner\src\g-c-spanner-build
+  ctest --output-on-failure -L integration-tests -j 1
+  if ($LastExitCode) {
+      throw "Integration tests failed with exit code $LastExitCode"
+  }
+}
---
+++ ci/kokoro/windows/build.bat
@@ -0,0 +1,58 @@
+REM Copyright 2018 Google LLC
+REM
+REM Licensed under the Apache License, Version 2.0 (the "License");
+REM you may not use this file except in compliance with the License.
+REM You may obtain a copy of the License at
+REM
+REM     http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM Unless required by applicable law or agreed to in writing, software
+REM distributed under the License is distributed on an "AS IS" BASIS,
+REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM See the License for the specific language governing permissions and
+REM limitations under the License.
+
+echo %date% %time%
+cd github\google-cloud-cpp-spanner
+
+set CONFIG=Debug
+set PROVIDER=package
+call "c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+echo %date% %time%
+cmd /c gcloud auth activate-service-account --key-file "%KOKORO_GFILE_DIR%/build-results-service-account.json"
+
+call "%KOKORO_GFILE_DIR%/spanner-integration-tests-config.bat"
+set GOOGLE_APPLICATION_CREDENTIALS="%KOKORO_GFILE_DIR%/spanner-credentials.json"
+set GOOGLE_CLOUD_CPP_AUTO_RUN_EXAMPLES=yes
+set GRPC_DEFAULT_SSL_ROOTS_FILE_PATH=T:\src\github\vcpkg\packages\grpc_x64-windows-static\share\grpc\roots.pem
+
+echo %date% %time%
+powershell -exec bypass ci\kokoro\windows\build-dependencies.ps1
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo %date% %time%
+cmd /c gcloud auth revoke --all
+
+echo %date% %time%
+powershell -exec bypass ci\kokoro\windows\build-project.ps1
+@rem Preserve the exit code of the test for later use because we want to
+@rem delete the files in the %KOKORO_ARTIFACTS_DIR% on test failure too.
+set test_errorlevel=%errorlevel%
+
+@rem Kokoro rsyncs all the files in the %KOKORO_ARTIFACTS_DIR%, which takes a
+@rem long time. The recommended workaround is to remove all the files that are
+@rem not interesting artifacts.
+if defined KOKORO_ARTIFACTS_DIR (
+  echo %date% %time%
+  cd "%KOKORO_ARTIFACTS_DIR%"
+  powershell -Command "& {Get-ChildItem -Recurse -File -Exclude test.xml,sponge_log.xml,build.bat | Remove-Item -Recurse -Force}"
+  if %errorlevel% neq 0 exit /b %errorlevel%
+)
+
+if %test_errorlevel% neq 0 exit /b %test_errorlevel%
+
+@echo %date% %time%
+@echo DONE DONE DONE "============================================="
+@echo DONE DONE DONE "============================================="
+@echo %date% %time%
