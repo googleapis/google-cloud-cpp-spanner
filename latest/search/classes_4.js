6078e30b0e60b8626f9fdef8fb7786dc66efe5d8
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-09-11 12:45:45 -0700

    Automatically generated documentation

--- latest/search/classes_4.js
+++ latest/search/classes_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['genericpollingpolicy',['GenericPollingPolicy',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1GenericPollingPolicy.html',1,'google::cloud::spanner::v0']]],
+  ['getiampolicyparams',['GetIamPolicyParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1InstanceAdminConnection_1_1GetIamPolicyParams.html',1,'google::cloud::spanner::v0::InstanceAdminConnection']]],
   ['getinstanceparams',['GetInstanceParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1InstanceAdminConnection_1_1GetInstanceParams.html',1,'google::cloud::spanner::v0::InstanceAdminConnection']]]
 ];
