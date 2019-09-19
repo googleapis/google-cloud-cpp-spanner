5e5889bf0bce06ee39cb1247fc35a21e7aac1416
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-09-19 12:37:43 -0700

    Automatically generated documentation

--- latest/search/classes_4.js
+++ latest/search/classes_4.js
@@ -3,6 +3,6 @@ var searchData=
   ['genericpollingpolicy',['GenericPollingPolicy',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1GenericPollingPolicy.html',1,'google::cloud::spanner::v0']]],
   ['getdatabaseddlparams',['GetDatabaseDdlParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminConnection_1_1GetDatabaseDdlParams.html',1,'google::cloud::spanner::v0::DatabaseAdminConnection']]],
   ['getdatabaseparams',['GetDatabaseParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminConnection_1_1GetDatabaseParams.html',1,'google::cloud::spanner::v0::DatabaseAdminConnection']]],
-  ['getiampolicyparams',['GetIamPolicyParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1InstanceAdminConnection_1_1GetIamPolicyParams.html',1,'google::cloud::spanner::v0::InstanceAdminConnection']]],
+  ['getiampolicyparams',['GetIamPolicyParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminConnection_1_1GetIamPolicyParams.html',1,'google::cloud::spanner::v0::DatabaseAdminConnection::GetIamPolicyParams'],['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1InstanceAdminConnection_1_1GetIamPolicyParams.html',1,'google::cloud::spanner::v0::InstanceAdminConnection::GetIamPolicyParams']]],
   ['getinstanceparams',['GetInstanceParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1InstanceAdminConnection_1_1GetInstanceParams.html',1,'google::cloud::spanner::v0::InstanceAdminConnection']]]
 ];
