432611476b8ca82d57f93c1651c9ae573d548216
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-09-20 14:18:37 -0700

    Automatically generated documentation

--- latest/search/all_9.js
+++ latest/search/all_9.js
@@ -2,7 +2,9 @@ var searchData=
 [
   ['limit',['limit',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1ReadOptions.html#a378a0619dfd5ffd2a906f57f71d993b7',1,'google::cloud::spanner::v0::ReadOptions']]],
   ['limitederrorcountretrypolicy',['LimitedErrorCountRetryPolicy',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#a261401d138e3aa98ea260e0fc1e95637',1,'google::cloud::spanner::v0']]],
+  ['limitederrorcounttransactionrerunpolicy',['LimitedErrorCountTransactionRerunPolicy',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#a6094ab0a6ec6c6fb30b4e3a6a9431a5c',1,'google::cloud::spanner::v0']]],
   ['limitedtimeretrypolicy',['LimitedTimeRetryPolicy',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#ab1f307578e2cae145104cae9703811a1',1,'google::cloud::spanner::v0']]],
+  ['limitedtimetransactionrerunpolicy',['LimitedTimeTransactionRerunPolicy',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#a0703e4c1046052758dcfb11f0fb0f2a6',1,'google::cloud::spanner::v0']]],
   ['listdatabaserange',['ListDatabaseRange',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#ab2103cd7782fc4748867fa8ad2b35e09',1,'google::cloud::spanner::v0']]],
   ['listdatabases',['ListDatabases',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminClient.html#ae787f480f6b2ecc3bee04c2b23428390',1,'google::cloud::spanner::v0::DatabaseAdminClient::ListDatabases()'],['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminConnection.html#a66c441b8690c734cf8670164f1104ac1',1,'google::cloud::spanner::v0::DatabaseAdminConnection::ListDatabases()']]],
   ['listdatabasesparams',['ListDatabasesParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminConnection_1_1ListDatabasesParams.html',1,'google::cloud::spanner::v0::DatabaseAdminConnection']]],
--- latest/search/typedefs_8.js
+++ latest/search/typedefs_8.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['timestamp',['Timestamp',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#a9711d66cf02a8305d987474ac7c5efca',1,'google::cloud::spanner::v0']]]
+  ['timestamp',['Timestamp',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#a9711d66cf02a8305d987474ac7c5efca',1,'google::cloud::spanner::v0']]],
+  ['transactionrerunpolicy',['TransactionRerunPolicy',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#a52b5d68feb45c40d761c79ee360b804c',1,'google::cloud::spanner::v0']]]
 ];
