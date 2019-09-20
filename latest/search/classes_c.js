75c636d513349c7a6298612a8db342dcd19a08cd
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-09-20 07:48:03 -0700

    Automatically generated documentation

--- latest/search/classes_c.js
+++ latest/search/classes_c.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['setiampolicyparams',['SetIamPolicyParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1InstanceAdminConnection_1_1SetIamPolicyParams.html',1,'google::cloud::spanner::v0::InstanceAdminConnection']]],
+  ['setiampolicyparams',['SetIamPolicyParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminConnection_1_1SetIamPolicyParams.html',1,'google::cloud::spanner::v0::DatabaseAdminConnection::SetIamPolicyParams'],['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1InstanceAdminConnection_1_1SetIamPolicyParams.html',1,'google::cloud::spanner::v0::InstanceAdminConnection::SetIamPolicyParams']]],
   ['singleuseoptions',['SingleUseOptions',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Transaction_1_1SingleUseOptions.html',1,'google::cloud::spanner::v0::Transaction']]],
   ['sqlstatement',['SqlStatement',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1SqlStatement.html',1,'google::cloud::spanner::v0']]],
   ['stats',['Stats',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1BatchDmlResult_1_1Stats.html',1,'google::cloud::spanner::v0::BatchDmlResult']]]
