97693222d0d5965788cfb6e64315ff1471d9747c
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-09-20 08:47:49 -0700

    Automatically generated documentation

--- latest/search/classes_d.js
+++ latest/search/classes_d.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['testiampermissionsparams',['TestIamPermissionsParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1InstanceAdminConnection_1_1TestIamPermissionsParams.html',1,'google::cloud::spanner::v0::InstanceAdminConnection']]],
+  ['testiampermissionsparams',['TestIamPermissionsParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminConnection_1_1TestIamPermissionsParams.html',1,'google::cloud::spanner::v0::DatabaseAdminConnection::TestIamPermissionsParams'],['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1InstanceAdminConnection_1_1TestIamPermissionsParams.html',1,'google::cloud::spanner::v0::InstanceAdminConnection::TestIamPermissionsParams']]],
   ['transaction',['Transaction',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Transaction.html',1,'google::cloud::spanner::v0']]]
 ];
