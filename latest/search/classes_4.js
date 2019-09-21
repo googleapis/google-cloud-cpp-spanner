10a3d296378669b9f2fe2d71255984d424675735
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-09-21 08:56:51 -0700

    Automatically generated documentation

--- latest/search/classes_4.js
+++ latest/search/classes_4.js
@@ -4,5 +4,6 @@ var searchData=
   ['getdatabaseddlparams',['GetDatabaseDdlParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminConnection_1_1GetDatabaseDdlParams.html',1,'google::cloud::spanner::v0::DatabaseAdminConnection']]],
   ['getdatabaseparams',['GetDatabaseParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminConnection_1_1GetDatabaseParams.html',1,'google::cloud::spanner::v0::DatabaseAdminConnection']]],
   ['getiampolicyparams',['GetIamPolicyParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminConnection_1_1GetIamPolicyParams.html',1,'google::cloud::spanner::v0::DatabaseAdminConnection::GetIamPolicyParams'],['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1InstanceAdminConnection_1_1GetIamPolicyParams.html',1,'google::cloud::spanner::v0::InstanceAdminConnection::GetIamPolicyParams']]],
+  ['getinstanceconfigparams',['GetInstanceConfigParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1InstanceAdminConnection_1_1GetInstanceConfigParams.html',1,'google::cloud::spanner::v0::InstanceAdminConnection']]],
   ['getinstanceparams',['GetInstanceParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1InstanceAdminConnection_1_1GetInstanceParams.html',1,'google::cloud::spanner::v0::InstanceAdminConnection']]]
 ];
