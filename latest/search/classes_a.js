f4a78036812f54d84234b68533027349c52adb87
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-10-24 11:33:01 -0700

    Automatically generated documentation

--- latest/search/classes_a.js
+++ latest/search/classes_a.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['querypartition',['QueryPartition',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1QueryPartition.html',1,'google::cloud::spanner::v0']]],
-  ['queryresult',['QueryResult',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1QueryResult.html',1,'google::cloud::spanner::v0']]]
+  ['querypartition',['QueryPartition',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1QueryPartition.html',1,'google::cloud::spanner::v0']]]
 ];
