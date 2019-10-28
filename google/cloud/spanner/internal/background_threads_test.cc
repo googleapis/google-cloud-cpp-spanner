0b7fb9879045656f818c9f3e46494ccd91657d0d
Carlos O'Ryan <coryan@google.com>
2019-10-28 11:31:03 -0400

    bug: fix (or reduce) flakiness of test
    This should at the very least reduce the flakiness of the test. It was
    failing because the thread handling a callback *can* be the same thread
    if the event (in the test "the timer") is already satisfied when the
    callback is registered.

--- google/cloud/spanner/internal/background_threads_test.cc
+++ google/cloud/spanner/internal/background_threads_test.cc
@@ -85,7 +85,18 @@ TEST(AutomaticallyCreatedBackgroundThreads, IsNotCurrentThread) {
 
   using ms = std::chrono::milliseconds;
 
-  future<std::thread::id> id = actual.cq().MakeRelativeTimer(ms(0)).then(
+  auto timer = actual.cq().MakeRelativeTimer(ms(10));
+  if (timer.is_ready()) {
+    // Under heavy load the timer might fire "immediately", that is, by the time
+    // we create the timer it has already expired. In that case the current
+    // thread fires the callback, as the timer is immediately satisfied and the
+    // test fails.
+    return;
+  }
+  // There is a small window between the call to `.is_ready()` and setting the
+  // callback where the timer could expire. If that becomes common we can think
+  // of a more complicated approach, or we can just disable this test.
+  future<std::thread::id> id = timer.then(
       [](future<std::chrono::system_clock::time_point>) {
         return std::this_thread::get_id();
       });
