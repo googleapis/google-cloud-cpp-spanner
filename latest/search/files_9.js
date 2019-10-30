6e5005aef717fa9d41ec909fcb0d60dac984262f
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-10-30 16:55:25 -0700

    Automatically generated documentation

--- latest/search/files_9.js
+++ latest/search/files_9.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['samples_2ecc',['samples.cc',['../samples_8cc.html',1,'']]],
+  ['single_5frow_5fthroughput_5fbenchmark_2ecc',['single_row_throughput_benchmark.cc',['../single__row__throughput__benchmark_8cc.html',1,'']]],
   ['spanner_2dmain_2edox',['spanner-main.dox',['../spanner-main_8dox.html',1,'']]],
   ['spanner_2dmocking_2edox',['spanner-mocking.dox',['../spanner-mocking_8dox.html',1,'']]],
   ['sql_5fstatement_2ecc',['sql_statement.cc',['../sql__statement_8cc.html',1,'']]],
