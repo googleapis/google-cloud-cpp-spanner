32f4e0b343456e719b4f2475ef2f7659fe0552f0
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-10-31 08:28:36 -0700

    Automatically generated documentation

--- latest/search/classes_d.js
+++ latest/search/classes_d.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['testiampermissionsparams',['TestIamPermissionsParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1InstanceAdminConnection_1_1TestIamPermissionsParams.html',1,'google::cloud::spanner::v0::InstanceAdminConnection::TestIamPermissionsParams'],['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminConnection_1_1TestIamPermissionsParams.html',1,'google::cloud::spanner::v0::DatabaseAdminConnection::TestIamPermissionsParams']]],
+  ['testiampermissionsparams',['TestIamPermissionsParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminConnection_1_1TestIamPermissionsParams.html',1,'google::cloud::spanner::v0::DatabaseAdminConnection::TestIamPermissionsParams'],['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1InstanceAdminConnection_1_1TestIamPermissionsParams.html',1,'google::cloud::spanner::v0::InstanceAdminConnection::TestIamPermissionsParams']]],
   ['transaction',['Transaction',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Transaction.html',1,'google::cloud::spanner::v0']]],
   ['tuplestreamiterator',['TupleStreamIterator',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1TupleStreamIterator.html',1,'google::cloud::spanner::v0']]]
 ];
