33438f2d5152df8c396ddcc4a176a544295c3f12
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-11-12 06:56:24 -0800

    Automatically generated documentation

--- latest/search/classes_c.js
+++ latest/search/classes_c.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['setiampolicyparams',['SetIamPolicyParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminConnection_1_1SetIamPolicyParams.html',1,'google::cloud::spanner::v0::DatabaseAdminConnection::SetIamPolicyParams'],['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1InstanceAdminConnection_1_1SetIamPolicyParams.html',1,'google::cloud::spanner::v0::InstanceAdminConnection::SetIamPolicyParams']]],
+  ['setiampolicyparams',['SetIamPolicyParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1InstanceAdminConnection_1_1SetIamPolicyParams.html',1,'google::cloud::spanner::v0::InstanceAdminConnection::SetIamPolicyParams'],['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminConnection_1_1SetIamPolicyParams.html',1,'google::cloud::spanner::v0::DatabaseAdminConnection::SetIamPolicyParams']]],
   ['singleuseoptions',['SingleUseOptions',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Transaction_1_1SingleUseOptions.html',1,'google::cloud::spanner::v0::Transaction']]],
   ['sqlparams',['SqlParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1Connection_1_1SqlParams.html',1,'google::cloud::spanner::v0::Connection']]],
   ['sqlstatement',['SqlStatement',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1SqlStatement.html',1,'google::cloud::spanner::v0']]],
