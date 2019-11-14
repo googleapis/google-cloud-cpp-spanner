d540b118697d2073fa4758eff310b2f2163b488c
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-11-14 10:53:21 -0800

    Automatically generated documentation

--- latest/search/files_5.js
+++ latest/search/files_5.js
@@ -4,6 +4,7 @@ var searchData=
   ['mock_5fexecute_5fquery_2ecc',['mock_execute_query.cc',['../mock__execute__query_8cc.html',1,'']]],
   ['mock_5finstance_5fadmin_5fconnection_2eh',['mock_instance_admin_connection.h',['../mock__instance__admin__connection_8h.html',1,'']]],
   ['mock_5fspanner_5fconnection_2eh',['mock_spanner_connection.h',['../mock__spanner__connection_8h.html',1,'']]],
+  ['multiple_5frows_5fcpu_5fbenchmark_2ecc',['multiple_rows_cpu_benchmark.cc',['../multiple__rows__cpu__benchmark_8cc.html',1,'']]],
   ['mutations_2ecc',['mutations.cc',['../mutations_8cc.html',1,'']]],
   ['mutations_2eh',['mutations.h',['../mutations_8h.html',1,'']]]
 ];
--- latest/search/files_9.js
+++ latest/search/files_9.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['samples_2ecc',['samples.cc',['../samples_8cc.html',1,'']]],
-  ['single_5frow_5fcpu_5fbenchmark_2ecc',['single_row_cpu_benchmark.cc',['../single__row__cpu__benchmark_8cc.html',1,'']]],
   ['single_5frow_5fthroughput_5fbenchmark_2ecc',['single_row_throughput_benchmark.cc',['../single__row__throughput__benchmark_8cc.html',1,'']]],
   ['spanner_2dmain_2edox',['spanner-main.dox',['../spanner-main_8dox.html',1,'']]],
   ['spanner_2dmocking_2edox',['spanner-mocking.dox',['../spanner-mocking_8dox.html',1,'']]],
