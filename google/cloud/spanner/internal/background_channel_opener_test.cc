3c5717ecb85c72aa75c78e70c8a84fc24441eed8
Takashi Matsuo <tmatsuo@google.com>
2019-11-22 23:58:52 -0800

    fix style

--- google/cloud/spanner/internal/background_channel_opener_test.cc
+++ google/cloud/spanner/internal/background_channel_opener_test.cc
@@ -98,23 +98,22 @@ TEST_F(BackgroundChannelOpenerTest, WithServer) {
   using ms = std::chrono::milliseconds;
   cq_.MakeRelativeTimer(ms(200)).then(
       [this](future<std::chrono::system_clock::time_point>) {
-       BackgroundChannelOpener(cq_, channel_);
+        BackgroundChannelOpener(cq_, channel_);
       });
   grpc_connectivity_state state = GRPC_CHANNEL_IDLE;
   int retry = 10;
   while (--retry > 0) {
-    channel_->WaitForStateChange(
-        state,
-        std::chrono::system_clock::now() + ms(1000));
+    channel_->WaitForStateChange(state,
+                                 std::chrono::system_clock::now() + ms(1000));
     state = channel_->GetState(false);
     if (state == GRPC_CHANNEL_READY || state == GRPC_CHANNEL_CONNECTING) {
       break;
     }
   }
   ASSERT_TRUE(state == GRPC_CHANNEL_READY || state == GRPC_CHANNEL_CONNECTING)
-            << "Where state: "   << state
-            << " not equal neither: " << GRPC_CHANNEL_READY
-            << " nor: "               << GRPC_CHANNEL_CONNECTING << ".";
+      << "Where state: " << state
+      << " not equal neither: " << GRPC_CHANNEL_READY
+      << " nor: " << GRPC_CHANNEL_CONNECTING << ".";
 }
 
 }  // namespace
