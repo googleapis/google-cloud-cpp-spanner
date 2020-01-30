e4a2fd6b15ed88fb95d3abdef58dba48944da3bf
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2020-01-30 11:00:01 -0800

    Automatically generated documentation

--- latest/search/classes_1.js
+++ latest/search/classes_1.js
@@ -3,6 +3,7 @@ var searchData=
   ['client',['Client',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Client.html',1,'google::cloud::spanner::v0']]],
   ['commitparams',['CommitParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1Connection_1_1CommitParams.html',1,'google::cloud::spanner::v0::Connection']]],
   ['commitresult',['CommitResult',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1CommitResult.html',1,'google::cloud::spanner::v0']]],
+  ['committimestamp',['CommitTimestamp',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1CommitTimestamp.html',1,'google::cloud::spanner::v0']]],
   ['connection',['Connection',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1Connection.html',1,'google::cloud::spanner::v0']]],
   ['connectionoptions',['ConnectionOptions',['../classgoogle_1_1cloud_1_1spanner_1_1v0_1_1ConnectionOptions.html',1,'google::cloud::spanner::v0']]],
   ['createdatabaseparams',['CreateDatabaseParams',['../structgoogle_1_1cloud_1_1spanner_1_1v0_1_1DatabaseAdminConnection_1_1CreateDatabaseParams.html',1,'google::cloud::spanner::v0::DatabaseAdminConnection']]],
