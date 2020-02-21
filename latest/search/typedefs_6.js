b362b9da257d30d6348f2a320a42e9b696bcf643
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2020-02-21 09:07:54 -0800

    Automatically generated documentation

--- latest/search/typedefs_6.js
+++ latest/search/typedefs_6.js
@@ -4,7 +4,7 @@ var searchData=
   ['limitederrorcounttransactionrerunpolicy',['LimitedErrorCountTransactionRerunPolicy',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#a6094ab0a6ec6c6fb30b4e3a6a9431a5c',1,'google::cloud::spanner::v0']]],
   ['limitedtimeretrypolicy',['LimitedTimeRetryPolicy',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#ab1f307578e2cae145104cae9703811a1',1,'google::cloud::spanner::v0']]],
   ['limitedtimetransactionrerunpolicy',['LimitedTimeTransactionRerunPolicy',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#a0703e4c1046052758dcfb11f0fb0f2a6',1,'google::cloud::spanner::v0']]],
-  ['listdatabaserange',['ListDatabaseRange',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#ab2103cd7782fc4748867fa8ad2b35e09',1,'google::cloud::spanner::v0']]],
-  ['listinstanceconfigsrange',['ListInstanceConfigsRange',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#a26a21e57b79a55456e5eb82ea075cdf9',1,'google::cloud::spanner::v0']]],
-  ['listinstancesrange',['ListInstancesRange',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#a71cb6e79b7fb96cdd1bf646b8ab8e09b',1,'google::cloud::spanner::v0']]]
+  ['listdatabaserange',['ListDatabaseRange',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#a26da85a6c1543ac1cabbb504cf6da973',1,'google::cloud::spanner::v0']]],
+  ['listinstanceconfigsrange',['ListInstanceConfigsRange',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#a4717eab0777ca2fd4980fe00847b3ea6',1,'google::cloud::spanner::v0']]],
+  ['listinstancesrange',['ListInstancesRange',['../namespacegoogle_1_1cloud_1_1spanner_1_1v0.html#a7505c57812676c54d6854fe8bc3d9e1a',1,'google::cloud::spanner::v0']]]
 ];
