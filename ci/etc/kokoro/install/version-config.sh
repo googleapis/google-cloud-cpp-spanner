685d0b7c62fd2e6ddc060e2c4b639b44bb9adc69
Carlos O'Ryan <coryan@users.noreply.github.com>
2020-03-18 07:51:27 -0400

    chore: upgrade g-c-cpp-common to v0.24.0 (#1411)

--- ci/etc/kokoro/install/version-config.sh
+++ ci/etc/kokoro/install/version-config.sh
@@ -13,4 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-readonly GOOGLE_CLOUD_CPP_COMMON_VERSION=0.23.0
+readonly GOOGLE_CLOUD_CPP_COMMON_VERSION=0.24.0
