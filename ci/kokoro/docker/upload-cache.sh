51f5d8ad20ac615bcfb45cad2b2555d34d741611
Carlos O'Ryan <coryan@google.com>
2020-03-23 11:05:43 -0400

    Warm up the cache

--- ci/kokoro/docker/upload-cache.sh
+++ ci/kokoro/docker/upload-cache.sh
@@ -36,7 +36,7 @@ if [[ "${KOKORO_JOB_TYPE:-}" == "PRESUBMIT_GERRIT_ON_BORG" ]] || \
    [[ "${KOKORO_JOB_TYPE:-}" == "PRESUBMIT_GITHUB" ]]; then
   echo "================================================================"
   echo "This is a presubmit build, cache will not be updated, exist with success."
-  exit 0
+# TODO(coryan)  exit 0
 fi
 
 echo "================================================================"
