6896a51353c68824589d5dc81488f6792cfd2dda
Todd Derr <salty@google.com>
2020-03-26 14:35:23 -0400

    feat: provide a mockable `Clock` class
    Like #1449 but different; this should make it easier to add operations
    in the future (like sleep/wait), at the cost of having to pass it around
    as a `shared_ptr` even for the `RealClock`.  (In #1449 we only needed
    the `shared_ptr` in the fake case.)
    
    fixes #1428

--- google/cloud/spanner/internal/session_pool.h
+++ google/cloud/spanner/internal/session_pool.h
@@ -18,6 +18,7 @@
 #include "google/cloud/spanner/backoff_policy.h"
 #include "google/cloud/spanner/database.h"
 #include "google/cloud/spanner/internal/channel.h"
+#include "google/cloud/spanner/internal/clock.h"
 #include "google/cloud/spanner/internal/session.h"
 #include "google/cloud/spanner/internal/spanner_stub.h"
 #include "google/cloud/spanner/retry_policy.h"
@@ -69,7 +70,8 @@ class SessionPool : public std::enable_shared_from_this<SessionPool> {
   SessionPool(Database db, std::vector<std::shared_ptr<SpannerStub>> stubs,
               SessionPoolOptions options, google::cloud::CompletionQueue cq,
               std::unique_ptr<RetryPolicy> retry_policy,
-              std::unique_ptr<BackoffPolicy> backoff_policy);
+              std::unique_ptr<BackoffPolicy> backoff_policy,
+              std::shared_ptr<Clock> clock);
 
   ~SessionPool();
 
@@ -165,6 +167,7 @@ class SessionPool : public std::enable_shared_from_this<SessionPool> {
   google::cloud::CompletionQueue cq_;
   std::unique_ptr<RetryPolicy const> retry_policy_prototype_;
   std::unique_ptr<BackoffPolicy const> backoff_policy_prototype_;
+  std::shared_ptr<Clock> clock_;
   int const max_pool_size_;
   std::mt19937 random_generator_;
 
@@ -176,8 +179,8 @@ class SessionPool : public std::enable_shared_from_this<SessionPool> {
   int num_waiting_for_session_ = 0;                 // GUARDED_BY(mu_)
 
   // Lower bound on all `sessions_[i]->last_use_time()` values.
-  Session::Clock::time_point last_use_time_lower_bound_ =
-      Session::Clock::now();  // GUARDED_BY(mu_)
+  Clock::time_point last_use_time_lower_bound_ =
+      clock_->Now();  // GUARDED_BY(mu_)
 
   future<void> current_timer_;
 
@@ -200,7 +203,8 @@ std::shared_ptr<SessionPool> MakeSessionPool(
     Database db, std::vector<std::shared_ptr<SpannerStub>> stubs,
     SessionPoolOptions options, google::cloud::CompletionQueue cq,
     std::unique_ptr<RetryPolicy> retry_policy,
-    std::unique_ptr<BackoffPolicy> backoff_policy);
+    std::unique_ptr<BackoffPolicy> backoff_policy,
+    std::shared_ptr<Clock> clock = std::make_shared<RealClock>());
 
 }  // namespace internal
 }  // namespace SPANNER_CLIENT_NS
