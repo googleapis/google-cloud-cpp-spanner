649175c870ac840276245d3dbdd1777c1691e234
Todd Derr <salty@google.com>
2020-03-26 14:35:23 -0400

    feat: provide a mockable `Clock` class
    Like #1449 but different; this should make it easier to add operations
    in the future (like sleep/wait), at the cost of having to pass it around
    as a `shared_ptr` even for the `RealClock`.  (In #1449 we only needed
    the `shared_ptr` in the fake case.)
    
    fixes #1428

---
+++ google/cloud/spanner/internal/clock.h
@@ -0,0 +1,78 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef GOOGLE_CLOUD_CPP_SPANNER_GOOGLE_CLOUD_SPANNER_INTERNAL_CLOCK_H
+#define GOOGLE_CLOUD_CPP_SPANNER_GOOGLE_CLOUD_SPANNER_INTERNAL_CLOCK_H
+
+#include "google/cloud/spanner/version.h"
+#include <chrono>
+#include <mutex>
+
+namespace google {
+namespace cloud {
+namespace spanner {
+inline namespace SPANNER_CLIENT_NS {
+namespace internal {
+
+/**
+ * A simple `Clock` class that can be overridden for testing.
+ *
+ * All implementations of this class are required to be thread-safe.
+ */
+class Clock {
+ public:
+  using time_point = std::chrono::steady_clock::time_point;
+  using duration = std::chrono::steady_clock::duration;
+
+  virtual ~Clock() = default;
+  virtual time_point Now() = 0;
+};
+
+class RealClock : public Clock {
+ public:
+  time_point Now() override { return std::chrono::steady_clock::now(); }
+};
+
+class FakeClock : public Clock {
+ public:
+  /// Construct a `FakeClock` with an optional starting `time_point` of `now`.
+  explicit FakeClock(time_point now = {}) { now_ = now; }
+  time_point Now() override {
+    std::lock_guard<std::mutex> lock(mu_);
+    return now_;
+  }
+
+  /// Sets the time to `now`.
+  void SetTime(time_point now) {
+    std::lock_guard<std::mutex> lock(mu_);
+    now_ = now;
+  }
+  /// Advances the time by `increment`.
+  void AdvanceTime(duration increment) {
+    std::lock_guard<std::mutex> lock(mu_);
+    now_ += increment;
+  }
+
+ private:
+  std::mutex mu_;
+  time_point now_;
+};
+
+}  // namespace internal
+}  // namespace SPANNER_CLIENT_NS
+}  // namespace spanner
+}  // namespace cloud
+}  // namespace google
+
+#endif  // GOOGLE_CLOUD_CPP_SPANNER_GOOGLE_CLOUD_SPANNER_INTERNAL_CLOCK_H
---
+++ google/cloud/spanner/internal/clock_test.cc
@@ -0,0 +1,57 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/spanner/internal/clock.h"
+#include <gmock/gmock.h>
+#include <memory>
+
+namespace google {
+namespace cloud {
+namespace spanner {
+inline namespace SPANNER_CLIENT_NS {
+namespace internal {
+namespace {
+
+TEST(Clock, RealClock) {
+  std::shared_ptr<Clock> clock = std::make_shared<RealClock>();
+  auto now = clock->Now();
+  auto now2 = clock->Now();
+  EXPECT_LE(now, now2);
+}
+
+TEST(Clock, FakeClock) {
+  RealClock real_clock;
+  Clock::time_point time(real_clock.Now());
+  FakeClock clock(time);
+  EXPECT_EQ(clock.Now(), time);
+
+  time += std::chrono::minutes(3);
+  clock.SetTime(time);
+  EXPECT_EQ(clock.Now(), time);
+
+  Clock::duration duration = std::chrono::hours(89);
+  time += duration;
+  clock.AdvanceTime(duration);
+  EXPECT_EQ(clock.Now(), time);
+  time += duration;
+  clock.AdvanceTime(duration);
+  EXPECT_EQ(clock.Now(), time);
+}
+
+}  // namespace
+}  // namespace internal
+}  // namespace SPANNER_CLIENT_NS
+}  // namespace spanner
+}  // namespace cloud
+}  // namespace google
