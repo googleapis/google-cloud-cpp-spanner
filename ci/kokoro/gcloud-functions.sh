57f84c8065d9c614aa79bdfb4ef04e607472b5a6
Carlos O'Ryan <coryan@google.com>
2020-04-14 12:51:30 -0400

    ci: avoid modifying the developer's gcloud config
    To upload and download the cache we need to add a service account to the
    developer's environment. For the CI builds this does not matter, as the
    CI runs with a clean environment. When running the builds locally this
    was annoying because it revoked the developer's credentials.
    
    These changes fix that, probably using more changes than strictily
    necessary, but it was easier to copy the code from g-c-cpp.

---
+++ ci/kokoro/gcloud-functions.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eu
+
+if [[ -z "${GCLOUD_CONFIG:-}" ]]; then
+  readonly GCLOUD_CONFIG="cloud-cpp-integration"
+  readonly GCLOUD_ARGS=(
+      # Do not seek confirmation for any actions, assume the default
+      "--quiet"
+
+      # Run the command using a custom configuration, this avoids affecting the
+      # user's `default` configuration
+      "--configuration=${GCLOUD_CONFIG}"
+  )
+fi
+
+activate_service_account_keyfile() {
+  local -r keyfile="$1"
+  local -r account="$(sed -n 's/.*"client_email": "\(.*\)",.*/\1/p' "${keyfile}")"
+  "${GCLOUD}" "${GCLOUD_ARGS[@]}" auth activate-service-account \
+      --key-file "${keyfile}" >/dev/null
+  "${GCLOUD}" "${GCLOUD_ARGS[@]}" config set account "${account}"
+}
+
+revoke_service_account_keyfile() {
+  local -r keyfile="$1"
+  local -r account="$(sed -n 's/.*"client_email": "\(.*\)",.*/\1/p' "${keyfile}")"
+  "${GCLOUD}" "${GCLOUD_ARGS[@]}" auth revoke "${account}" >/dev/null
+}
+
+delete_gcloud_config() {
+  "${GCLOUD}" --quiet config configurations delete "${GCLOUD_CONFIG}"
+}
+
+create_gcloud_config() {
+  echo
+  echo "================================================================"
+  if ! "${GCLOUD}" --quiet config configurations \
+           describe "${GCLOUD_CONFIG}" >/dev/null 2>&1; then
+    log_normal "Create the gcloud configuration for the cloud-cpp tests."
+    "${GCLOUD}" --quiet --no-user-output-enabled config configurations \
+        create --no-activate "${GCLOUD_CONFIG}" >/dev/null
+  fi
+  if [[ -n "${GOOGLE_CLOUD_PROJECT:-}" ]]; then
+    "${GCLOUD}" "${GCLOUD_ARGS[@]}" config set project "${GOOGLE_CLOUD_PROJECT}"
+  fi
+}
